ecb335d09cc5154ee454601d9cc7c9221468a9ee
BMLN <lorandbanki@gmail.com>
2022-06-26 20:11:35 +0200

    finished serial reader, WIP: action interface

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 - fill in the .env file with the information from the arduino ide
 - run:
-    pipenv install
-    pipenv run start
+
+        pipenv install
+        pipenv run start
--- reader.py
+++ reader.py
@@ -1,16 +1,65 @@
 import os
 import serial
+import threading
 
 
 
+class Reader(threading.Thread):
+    def __init__(self, monitor):
+        threading.Thread.__init__(self)
+        self.monitor = monitor
+        self.should_run = True
+        self.output = []
+
+    #uglyaf
+    def __sanitize(self):
+        self.output = [ x.decode("utf-8").replace("\r\n", "").split(',') for x in self.output ]
+        self.output = list(filter(lambda line : len(line) == 8 and str(line[0])[0] == '#' and str(line[-1])[-1] == '#' , self.output))
+        self.output = [ ";".join(x[1:4] + x[5:]).replace('#', '') for x in self.output ]
+
+    def run(self):
+        while self.should_run:
+            self.output.append(monitor.readline())
+        
+        self.__sanitize()
+
+
+
+
+
+
+
+status = 0
+name_index = 0
 monitor = serial.Serial(port=os.environ["ARD_PORT"], baudrate=os.environ["ARD_BAUD"], timeout=.1)
 
-x = 0
-while True and x < 20:
-    print(monitor.readline())
-    x += 1
+
+while status != "quit":
+    status = input("start/quit: ")
+
+    if status == "start":
+        #read
+        monitor.flushInput()
+        monitor.flushOutput()
+        output = []
+
+        recording = Reader(monitor)
+        recording.start() 
+
+        input("recording...\n'Enter' to end recording\n")
+        recording.should_run = False
+        recording.join()
+
+
+        #write
+        while os.path.exists("./" + str(name_index) + ".csv"):
+            name_index += 1
+
+        f = open(str(name_index) + ".csv", "w")
+        f.write("accX;accY;accZ;gyroX;gyroY;gyroZ\n")
+        for line in recording.output:
+            f.write(str(line) + "\n")
+        f.close()
 
 monitor.close()
-
-
 exit()
